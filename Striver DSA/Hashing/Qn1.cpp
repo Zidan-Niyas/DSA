89771ee8ddbd1d5d5c3f40b5c8669b8fc150b0c7
Zidan-Niyas <87656623+Zidan-Niyas@users.noreply.github.com>
2023-08-15 11:16:51 +0530

    Update Qn1.cpp

--- Striver DSA/Hashing/Qn1.cpp
+++ Striver DSA/Hashing/Qn1.cpp
@@ -1,3 +1,5 @@
+// Problem Statement : you are given an array of length n with values from 1 to 'x' . Count the frequency of all the elements from 1 to 'n'.
+
 vector<int> countFrequency(int n, int x, vector<int> &nums){
     vector<int> ans;
     int hash[100000] = {0};
