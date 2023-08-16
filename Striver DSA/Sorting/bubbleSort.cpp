6adb015907c42e79f46a738f91f76faa5cfd4dfa
Zidan Niyas <87656623+Zidan-Niyas@users.noreply.github.com>
2023-08-16 21:42:57 +0530

    Update bubbleSort.cpp

--- Striver DSA/Sorting/bubbleSort.cpp
+++ Striver DSA/Sorting/bubbleSort.cpp
@@ -1,14 +1,19 @@
 // Bubble Sort (Logic) ---> Push the largest element to the last using adjacent swapping
+// Time Complexity = O(N^2) ---> Worst & Average Case
+// Time Complexity = O(N) ---> Best Case (Already sorted array)
 
 void bubbleSort(int arr[], int n) 
 {
+    int didSwap = 0;
     for(int i=0; i<n-1; i++) {
         for(int j=0; j<n-i-1; j++) {
-            if(arr[j] > arr[j+1]) {
+            if(arr[j] > arr[j+1]) { 
+                didSwap = 1;
                 int temp = arr[j];
                 arr[j] = arr[j+1];
                 arr[j+1] = temp;
             }
+            if(didSwap == 0) break;   // Best Case ---> TC = 0(N) 
         }
     }
 }
