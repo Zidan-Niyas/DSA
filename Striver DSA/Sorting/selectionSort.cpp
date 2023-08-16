75515e32b8e16724292dce1964bc1baafecd6055
Zidan Niyas <87656623+Zidan-Niyas@users.noreply.github.com>
2023-08-16 21:11:18 +0530

    Create selectionSort.cpp

---
+++ Striver DSA/Sorting/selectionSort.cpp
@@ -0,0 +1,21 @@
+// Selection Sort ---> Select minimum (logic)
+
+
+void swap(int i, int j, vector<int> &arr) {
+    int temp = arr[i];
+    arr[i] = arr[j];
+    arr[j] = temp;
+}
+
+
+void selectionSort(vector<int>&arr) {
+    // Write your code here.
+    int n = arr.size();
+    for(int i=0; i<n-1; i++) {
+        for(int j=i; j<n; j++) {
+          if (arr[j] < arr[i]) {
+            swap(i, j, arr);
+          }
+        }
+    }
+}
