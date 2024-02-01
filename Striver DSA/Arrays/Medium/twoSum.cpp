48dc8d64f297f4285e5b7c1a6129faa2b9dc4c71
Zidan Niyas <87656623+Zidan-Niyas@users.noreply.github.com>
2024-02-01 07:04:15 +0530

    Create twoSum.cpp

---
+++ Striver DSA/Arrays/Medium/twoSum.cpp
@@ -0,0 +1,52 @@
+//------------------ Brute Force (solved in 6 mins) ---------------//
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        vector<int> ans;
+        for(int i=0; i<nums.size(); i++) {
+            for(int j=i+1; j<nums.size(); j++) {
+                int sum = nums[i] + nums[j];
+                if(sum == target) {
+                    ans.push_back(i);
+                    ans.push_back(j);
+                }
+            }
+        }
+    return ans;
+    }
+};
+
+//---------------- Optimized (two pass Hash Table) ---------------------//
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        unordered_map<int, int> mp;
+        for(int i=0; i<nums.size(); i++) {
+            int complement = target - nums[i];
+            if(mp.find(complement) == mp.end()) 
+                mp[nums[i]] = i;        // if complement not present in the map, add the element and index to the map
+            else 
+                return {mp[complement], i};   // if complement is present, index of complement and current value index is the answer
+            
+        }
+        return {-1,-1};
+    }
+};
+
+//--------------- Optimized (one pas Hash Table) --------------------//
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        unordered_map<int, int> mp;
+        for(int i=0; i<nums.size(); i++) {
+            int complement = target - nums[i];
+            if(mp.count(complement)) {      // count function counts the number of times complement occurs and returns
+                return {mp[complement], i}; // 1 if it exists and 0 if it does not. So if complement exists return.
+            }
+            else {
+                mp[nums[i]] = i;
+            }
+        }
+        return {-1,-1};    // if two numbers with sum = target doesnt exist return -1,-1
+    }
+};
