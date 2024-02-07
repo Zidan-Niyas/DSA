387e5be288ae140dffd783420c5d36a4c150e325
Zidan Niyas <87656623+Zidan-Niyas@users.noreply.github.com>
2024-02-07 07:29:56 +0530

    Create buyAndSellStock.cpp

---
+++ Striver DSA/Arrays/Medium/buyAndSellStock.cpp
@@ -0,0 +1,14 @@
+//---------------- Optimized Approach (passes all test cases) ----------------------//
+// TC = O(N) , SC = O(1)
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+        int maxProf = 0;
+        int minPrice = prices[0];
+        for(int i=1; i<prices.size(); i++) {
+            maxProf = max(maxProf, prices[i] - minPrice);
+            minPrice = min(minPrice, prices[i]);
+        }
+        return maxProf;
+    }
+};
